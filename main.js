fc33764d3a54d1c45d0a71f934f88a5db0d66fe6
FelipeGabrielEscola <132252340+FelipeGabrielEscola@users.noreply.github.com>
2023-10-09 12:14:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 function tocaSomPom(){
     document.querySelector(dElementoAudio).play();
    function tocaSom(idElementoAudio){}
-
- 
   const listaDeTeclas = document.querySelectorA11('.tecla');
-  
       let contador = 0;
         listaDeTeclas[0].onclick = tocaSomPom;
   // Estrutura de repetição -Enquanto
