377765a70c80726ebca6a245cf958bd4837b5fa2
FelipeGabrielEscola <132252340+FelipeGabrielEscola@users.noreply.github.com>
2023-10-23 12:11:37 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 function tocaSomPom(idElementoAudio){
   document.querySelector (idElementoAudio).play();
+const listaDeTeclas = document.querySelectorA11('.tecla');
 }
-
-
-    const listaDeTeclas = document.querySelectorA11('.tecla');
-
-    let contador = 0;
-    //Estrutura de reptição - Enquanto
     
-      while(contador <listaDeTeclas.length){
-        listaDeTeclas[0].onclick = tocaSomPom;
+    
+    //Estrutura de repetição - Enquanto
+      for(let contador = 0;contador < listaDeTeclas.length;contador++){
         const tecla = listaDeTeclas[contador]
-                const instrumento = tecla.classList[1];
-              const idAudio = `#som_ ${instrumento}`;
-                console.log(instrumento);
-           tecla.onclick = function(){
-            tocaSom('#_tecla_splash');
+        const instrumento = tecla.classList[1];
+        const idAudio = `#som_ ${instrumento}`;
+        console.log(idAudio);
+           
+        tecla.onclick = function(){
+            tocaSom(idAudio);
            };
-           contador= contador + 1;
-            console.log(contador);
-          }
+        tecla.onkeydown = function(){
+          tecla.classList.add('ativa');
+        }
+        tecla.onkeyup = function(){
+          tecla.classList.remove('ativa');
+        }
+        console.log(contador);
+        }
       
   
